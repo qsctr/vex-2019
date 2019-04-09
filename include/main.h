275438b412f41ef2eb1044d1ee0e41a782717711
Bretton <qsctr@users.noreply.github.com>
2019-04-09 16:29:20 +0800

    Un-vendor main.h

--- include/main.h
+++ include/main.h
@@ -1,3 +1,17 @@
+/**
+ * \file main.h
+ *
+ * Contains common definitions and header files used throughout your PROS
+ * project.
+ *
+ * Copyright (c) 2017-2018, Purdue University ACM SIGBots.
+ * All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef _PROS_MAIN_H_
 #define _PROS_MAIN_H_
 
@@ -9,7 +23,7 @@
 
 using namespace okapi;
 
-
+#include "robot.h"
 
 #ifdef __cplusplus
 extern "C" {
